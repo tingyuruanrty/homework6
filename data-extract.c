21e2749dcfd7b149a16696aa9417e2f0afd3856e
tingyuruanrty <122615937+tingyuruanrty@users.noreply.github.com>
2025-11-28 04:03:00 +0000

    finish data extract main

--- data-extract.c
+++ data-extract.c
@@ -7,7 +7,7 @@ int copy_integers(FILE *outfp, FILE *infp, int start, int end)
     /* TODO: Copy integers from start to end. 
 	     You may like to use the functions fseek(), fread() and fwrite() to accomplish this task. Check man pages for details.
 	     Return 0 upon successful completion. Otherwise, return -1. */	
-
+    
     return 0;
 }
 
@@ -21,30 +21,48 @@ int main(int argc, char *argv[])
     FILE *infp, *outfp;
 
     /* TODO: Open the input and output files for read and write operations respectively. If the operations fail, return -1. */
+    infp = fopen(argv[1], "r+");
+    if (infp == NULL){
+        printf("open inputfile failed\n");
+        return -1;
+    }
 
+    outfp = fopen(argv[2], "w+");
+    if (outfp == NULL){
+        printf("open outputfile failed\n");
+        return -1;
+    }
 
     for (int i=3; i<argc; i++) {
         int start, end;
         if (sscanf(argv[i], "%d-%d", &start, &end) != 2) {
             if (sscanf(argv[i], "%d", &start) != 1) {
-		printf("Invalid range!\n");
-		return -1;
-	    } 
+		        printf("Invalid range!\n");
+		        return -1;
+	        } 
             end = start;
         }
 
         if (start < 0 || end < 0 || end < start) {
-	  printf("Start and end must be >= 0 and start must be <= end\n");
-	  return -1;
-	}
+	        printf("Start and end must be >= 0 and start must be <= end\n");
+	        return -1;
+	    }
         if (copy_integers(outfp, infp, start, end)) {
-	  printf("copy_integers() returned an error.\n");
-	  return -1;
-	}
+	        printf("copy_integers() returned an error.\n");
+	        return -1;
+	    }
+
+
     } 
 
     /* TODO: Close the files. If the operations fail, return -1. */
-
-
+    if(fclose(infp)==EOF){
+        printf("close inputfile failed\n");
+        return -1;
+    }
+    if(fclose(outfp)==EOF){
+        printf("open outputfile failed\n");
+        return -1;
+    }
     return 0;
 }
