f566127539920122d1d25b60df6d2ec2dbc7bec0
Tingyu Ruan <tingyuruanrty@gmail.com>
2025-11-27 21:16:25 -0800

    my code

--- data-extract.c
+++ data-extract.c
@@ -4,24 +4,29 @@
 /* This function copies integers from file infp to file outfp. */
 int copy_integers(FILE *outfp, FILE *infp, int start, int end)
 {
-    /* TODO: Copy integers from start to end. 
-	     You may like to use the functions fseek(), fread() and fwrite() to accomplish this task. Check man pages for details.
-	     Return 0 upon successful completion. Otherwise, return -1. */	
-    
+    /* TODO: Copy integers from start to end.
+         You may like to use the functions fseek(), fread() and fwrite() to accomplish this task. Check man pages for details.
+         Return 0 upon successful completion. Otherwise, return -1. */
+
     int buffer;
 
-    if(fseek(infp, sizeof(int)*start, SEEK_SET)!=0){
+    if (fseek(infp, 4 * start, SEEK_SET) != 0)
+    {
         return -1;
     }
-    if(fseek(outfp, sizeof(int)*start, SEEK_SET)!=0){
+    if (fseek(outfp, 4 * start, SEEK_SET) != 0)
+    {
         return -1;
     }
 
-    for(int i = start; i < end+1;++i){
-        if(fread(&buffer,sizeof(int),1, infp)!=sizeof(int)){
+    for (int i = start; i < end + 1; ++i)
+    {
+        if (fread(&buffer, 4, 1, infp) != 4)
+        {
             return -1;
         }
-        if(fwrite(&buffer,sizeof(int),1, outfp)!=sizeof(int)){
+        if (fwrite(buffer, 4, 1, outfp) != 4)
+        {
             return -1;
         }
     }
@@ -30,7 +35,8 @@ int copy_integers(FILE *outfp, FILE *infp, int start, int end)
 
 int main(int argc, char *argv[])
 {
-    if (argc < 4) {
+    if (argc < 4)
+    {
         printf("Usage: %s <input-filename> <output-filename> range [range ..]\n", argv[0]);
         return -1;
     }
@@ -39,45 +45,52 @@ int main(int argc, char *argv[])
 
     /* TODO: Open the input and output files for read and write operations respectively. If the operations fail, return -1. */
     infp = fopen(argv[1], "r+");
-    if (infp == NULL){
+    if (infp == NULL)
+    {
         printf("open inputfile failed\n");
         return -1;
     }
 
     outfp = fopen(argv[2], "w+");
-    if (outfp == NULL){
+    if (outfp == NULL)
+    {
         printf("open outputfile failed\n");
         return -1;
     }
 
-    for (int i=3; i<argc; i++) {
+    for (int i = 3; i < argc; i++)
+    {
         int start, end;
-        if (sscanf(argv[i], "%d-%d", &start, &end) != 2) {
-            if (sscanf(argv[i], "%d", &start) != 1) {
-		        printf("Invalid range!\n");
-		        return -1;
-	        } 
+        if (sscanf(argv[i], "%d-%d", &start, &end) != 2)
+        {
+            if (sscanf(argv[i], "%d", &start) != 1)
+            {
+                printf("Invalid range!\n");
+                return -1;
+            }
             end = start;
         }
 
-        if (start < 0 || end < 0 || end < start) {
-	        printf("Start and end must be >= 0 and start must be <= end\n");
-	        return -1;
-	    }
-        if (copy_integers(outfp, infp, start, end)) {
-	        printf("copy_integers() returned an error.\n");
-	        return -1;
-	    }
-
-
-    } 
+        if (start < 0 || end < 0 || end < start)
+        {
+            printf("Start and end must be >= 0 and start must be <= end\n");
+            return -1;
+        }
+        if (copy_integers(outfp, infp, start, end))
+        {
+            printf("copy_integers() returned an error.\n");
+            return -1;
+        }
+    }
 
     /* TODO: Close the files. If the operations fail, return -1. */
-    if(fclose(infp)==EOF){
+    if (fclose(infp) == EOF)
+    {
         printf("close inputfile failed\n");
         return -1;
     }
-    if(fclose(outfp)==EOF){
+    if (fclose(outfp) == EOF)
+    {
         printf("open outputfile failed\n");
         return -1;
     }
