9e649603c5cacbae2e2e14c3e0d3c97b68533fa3
Tingyu Ruan <tingyuruanrty@gmail.com>
2025-11-27 17:09:37 -0800

    first commit

---
+++ README.txt
@@ -0,0 +1,16 @@
+3. Explain your answers (10 points)
+
+Please answer the following questions for each file you wrote as part of this assignment.
+
+1. my-grep
+"Why This Works" Explanation: Explain the underlying logic and reasoning that makes your code solve the problem. 
+Focus on the core concepts and principles your solution utilizes.
+
+
+
+2. data-extract
+"Why This Works" Explanation: Explain the underlying logic and reasoning that makes your code solve the problem. 
+Focus on the core concepts and principles your solution utilizes.
+
+
+
---
+++ mygrep.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+
+#define LINE_SIZE 1024
+
+/* This function prints lines (in a file) that contain string s. Assume all lines have at most (LINE_SIZE - 2) ASCII characters. */
+int print_matched_lines(const char *s, const char *filename) {
+    /* TODO: Open the file given in the argument and read every line till EOF. 
+	     If a line contains the string s, print it on the stdout. 
+             You may use strstr to determine if a string appears in another string. Check man page for strstr for details.
+	     Return 0 upon successful completion. Otherwise, return -1. */
+
+    FILE* fileWeRead = fopen(filename, "r");
+
+    if(fileWeRead == NULL){
+        fclose(fileWeRead);
+        return -1;
+    }
+
+    char* stringHolder;
+    while(fgets(stringHolder, LINE_SIZE, fileWeRead) != NULL){
+        if(strstr(stringHolder,s) != NULL){
+            printf("%s\n", stringHolder);
+        }
+    }
+
+    fclose(fileWeRead);
+    
+    return 0;
+}
+
+int main(int argc, char **argv) {
+    if (argc != 3) {
+        printf("Usage: %s <string> <filename>\n", argv[0]);
+        return -1;
+    }
+
+    if (print_matched_lines(argv[1], argv[2])) {
+        printf("print_matched_lines() returned a non_zero value.\n");
+	return -1;
+    }
+    return 0;
+}
